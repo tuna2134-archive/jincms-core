8a594c6fb7fbf7a3a9498b6a4af4445014bdda38
mc_fdc <Masato.04.11.2007@gmail.com>
2023-09-02 06:13:46 +0000

    shit

--- frontend/app/header.tsx
+++ frontend/app/header.tsx
@@ -2,6 +2,40 @@
 import React from "react";
 import Link from 'next/link'
 import { parseCookies } from "nookies";
+import { fetchUser, User } from "./_components/user";
+
+const UserMenu = ({ userid }: { userid: string }) => {
+  const [open, setOpen] = React.useState(false);
+  const [user, setUser] = React.useState<User | undefined>(undefined);
+  React.useEffect(() => {
+    (async () => {
+      const token = parseCookies().token;
+      if (!token) {
+        return;
+      } else {
+        const userData = await fetchUser(token);
+        setUser(userData);
+      }
+    })();
+  }, [setUser]);
+  return (
+    <div className="relative">
+      <button onClick={() => setOpen(!open)} className="rounded-full h-12 w-12 bg-gray-200 flex items-center justify-center">
+        <p className="text-2xl">{userid[0]}</p>
+      </button>
+      {open && (
+        <div className="absolute right-0 top-12 border rounded bg-white">
+          <div className="p-2 border-b">
+            <p className="text-2xl">{userid}</p>
+          </div>
+          <div className="p-2">
+            <Link href="/logout" className="text-2xl">Logout</Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export const Header = () => {
   const [user, setUser] = React.useState<User | undefined>(undefined);
@@ -12,10 +46,7 @@ export const Header = () => {
     } else {
       (async () => {
         const userData = await fetchUser(token);
-        if (userData !== undefined) {
-          userData.login = userData?.login.replaceAll('"', '');
-          setUser(userData);
-        };
+        setUser(userData);
       })();
     }
   });
