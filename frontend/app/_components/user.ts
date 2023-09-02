2cf5027d24cf8c9ace31024981656f591e84a137
mc_fdc <Masato.04.11.2007@gmail.com>
2023-09-02 06:40:23 +0000

    dsa

--- frontend/app/_components/user.ts
+++ frontend/app/_components/user.ts
@@ -9,7 +9,8 @@ export async function fetchUser(token: string): Promise<User | undefined> {
   const res = await fetch("http://localhost:8080/users/me", {
     headers: {
       Authorization: `Bearer ${token}`
-    }
+    },
+    method: "GET"
   });
   if (!res.ok) {
     return undefined;
--- frontend/app/header.tsx
+++ frontend/app/header.tsx
@@ -4,37 +4,42 @@ import Link from 'next/link'
 import { parseCookies } from "nookies";
 import { fetchUser, User } from "./_components/user";
 
+interface GithubUser {
+  login: string;
+  avatar_url: string;
+};
+
 const UserMenu = ({ userid }: { userid: string }) => {
-  const [open, setOpen] = React.useState(false);
-  const [user, setUser] = React.useState<User | undefined>(undefined);
+  const [isOpen, setOpen] = React.useState(false);
+  const [user, setUser] = React.useState<GithubUser | undefined>(undefined);
+  const openModal = () => setOpen(!isOpen);
   React.useEffect(() => {
     (async () => {
-      const token = parseCookies().token;
-      if (!token) {
-        return;
-      } else {
-        const userData = await fetchUser(token);
-        setUser(userData);
-      }
+      const res = await fetch(`https://api.github.com/users/${userid}`);
+      const data = await res.json();
+      console.log(data);
+      setUser(data);
     })();
   }, [setUser]);
   return (
-    <div className="relative">
-      <button onClick={() => setOpen(!open)} className="rounded-full h-12 w-12 bg-gray-200 flex items-center justify-center">
-        <p className="text-2xl">{userid[0]}</p>
+    <div>
+      <button onClick={openModal}>
+        <img src={user?.avatar_url} className="w-10 h-10 rounded-full inline-block" />
       </button>
-      {open && (
-        <div className="absolute right-0 top-12 border rounded bg-white">
-          <div className="p-2 border-b">
-            <p className="text-2xl">{userid}</p>
-          </div>
-          <div className="p-2">
-            <Link href="/logout" className="text-2xl">Logout</Link>
+      {isOpen &&
+        <div className="absolute border w-48 rounded">
+          <div className="bg-white">
+            <div className="flex flex-col">
+              <Link href="/dashboard" className="p-2 text-center border-b">Dashboard</Link>
+              <button className="p-2">
+                <p className="text-[#fc0303]">Logout</p>
+              </button>
+            </div>
           </div>
         </div>
-      )}
+      }
     </div>
-  );
+  )
 };
 
 export const Header = () => {
@@ -46,6 +51,10 @@ export const Header = () => {
     } else {
       (async () => {
         const userData = await fetchUser(token);
+        if (!userData) {
+          return;
+        }
+        userData.login = userData.login.replaceAll('"', '');
         setUser(userData);
       })();
     }
@@ -54,7 +63,7 @@ export const Header = () => {
     <header className="border-b">
       <div className='max-w-4xl w-full mx-auto h-16 items-center flex'>
         <div className='flex'>
-          <p className='text-4xl'>jincms</p>
+          <Link className='text-4xl' href="/">jincms</Link>
         </div>
         <div className='ml-auto items-center flex'>
           {user ?
