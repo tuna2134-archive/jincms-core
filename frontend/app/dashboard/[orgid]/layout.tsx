49ebca68744701ade489d5f3f8ee4b62fd25cb29
mc_fdc <me@tuna2134.dev>
2023-09-06 09:00:20 +0900

    Update layout.tsx

--- frontend/app/dashboard/[orgid]/layout.tsx
+++ frontend/app/dashboard/[orgid]/layout.tsx
@@ -23,8 +23,8 @@ export default function Page({
   ];
   return (
     <div className="max-w-6xl mx-auto w-full">
-      <div className="w-full flex min-h-screen relative">
-        <div className="fixed top-0 left-0 right-0 w-1/4 border-r overscroll-y-auto">
+      <div className="w-full flex min-h-screen">
+        <div className="w-1/4 border-r overscroll-y-auto">
           {settings.map((setting) => (
             <div key={setting.id} className="py-4 px-6 border-b">
               <Link href={setting.id} className="text-2xl">
